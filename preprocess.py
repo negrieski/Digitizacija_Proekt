ecce45e0ee51fbf2b85deaebe183ad5fabc6a646
Aleksandar Negrieski <negrieskialeksandar@gmail.com>
2026-08-09 02:04:31 +0200

    Enhance image preprocessing with Sauvola thresholding
    Added Gaussian blur and Sauvola thresholding for improved image preprocessing. Implemented connected components analysis to clean up the binary image.

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+import numpy as np
+from skimage.filters import threshold_sauvola
 
 input_folder = "images"
 output_folder = "preprocessed_images"
@@ -15,8 +17,34 @@ for filename in os.listdir(input_folder):
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        output_path = os.path.join(output_folder, filename)
+        blurred = cv2.GaussianBlur(gray, (3,3),0)
 
-        cv2.imwrite(output_path,gray)
+        # 2. Sauvola thresholding (интелигентна бинаризација за OCR)
+        window_size = 25
+        thresh_sauvola = threshold_sauvola(blurred, window_size=window_size, k=0.2)
+        binary = (blurred > thresh_sauvola).astype(np.uint8) * 255
+        inverted = cv2.bitwise_not(binary)
+
+        num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(inverted)
+
+        cleaned = np.zeros_like(inverted)
+
+
+        for i in range(1,num_labels):
+            area = stats[i, cv2.CC_STAT_AREA]
+            w = stats[i, cv2.CC_STAT_WIDTH]
+            h = stats[i, cv2.CC_STAT_HEIGHT]
+
+            if 5 <= area <= 4000 and w<250 and h<250:
+                cleaned [ labels == i] = 255
+
+
+
+
+        final_binary = cv2.bitwise_not(cleaned)
+
+        output_path = os.path.join(output_folder, filename)
+        cv2.imwrite(output_path, final_binary)
 
 print("Finished")
+
